5258cb5259f76d254ebdd2fb3be43b0ebe7aa126
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-29 15:15:52 +0700

    create Menu and MenuItem models

--- menu_app/models.py
+++ menu_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=100)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu_items', blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.title
